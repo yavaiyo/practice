a477084e333718b9d3aba1e55c7eb1ede12c374a
yavaiyo <bsns.gh.etc@gmail.com>
2024-07-19 17:22:45 +0900

    途中

--- header_kadai/header.js
+++ header_kadai/header.js
@@ -1,6 +1,14 @@
-const Open = document.querySelector('menu-open');
-const Menu = document.querySelector('rightlink');
+const Open = document.querySelector('.menu-open');
+const Menu = document.querySelector('.rightlink');
+const Close = document.querySelector('.close');
+const Back = document.querySelector('.background');
 
 Open.addEventListener('click', () => {
-    Menu.classList.toggle('active');
+    Menu.classList.add('active');
+    Back.classList.add('active');
+});
+
+Close.addEventListener('click', () => {
+    Menu.classList.remove('active');
+    Back.classList.remove('active');
 });
