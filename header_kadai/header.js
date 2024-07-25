ede01cc6fa13b428050b108bdfa8fcbb113c9e1b
yavaiyo <bsns.gh.etc@gmail.com>
2024-07-26 01:44:25 +0900

    完成

--- header_kadai/header.js
+++ header_kadai/header.js
@@ -12,3 +12,25 @@ Close.addEventListener('click', () => {
     Menu.classList.remove('active');
     Back.classList.remove('active');
 });
+
+class Resize {
+
+    constructor(target) {
+      this.timeoutId;
+      this.target = document.querySelector(target);
+   
+       window.addEventListener("resize", this._resize.bind(this))
+     }
+   
+     _resize() {
+       this.target.classList.add("is-resize");
+       clearTimeout(this.timeoutId);
+       
+       this.timeoutId = setTimeout(() => {
+         this.target.classList.remove("is-resize");
+       }, 500);
+     }
+   
+   }
+ 
+   new Resize("html");
