e97abeeede30128cc08059cdef1790c211edb1d8
yavaiyo <bsns.gh.etc@gmail.com>
2024-08-04 11:16:57 +0900

    修正完了

--- header_kadai/header.js
+++ header_kadai/header.js
@@ -13,24 +13,49 @@ Close.addEventListener('click', () => {
     Back.classList.remove('active');
 });
 
-class Resize {
+const mediaQueryList = matchMedia('(max-width:991px)');
 
-    constructor(target) {
-      this.timeoutId;
-      this.target = document.querySelector(target);
+mediaQueryList.addEventListener('change', (e) => {
+    if (e.matches) {
+      //pare3
+      document.querySelector('.rightlink').style.display = 'none';
+      setTimeout(() => {
+        document.querySelector('.rightlink').style.display = 'block';
+      }, 100);
+      //pare2
+        // Menu.classList.add('size');
+        // setTimeout(() => {
+        //   Menu.classList.remove('size');
+        // }, 200);
+    } 
+    //part3
+    else {
+      document.querySelector('.rightlink').style.display = 'flex';
+      setTimeout(() => {
+        document.querySelector('.rightlink').style.display = 'flex';
+      }, 200);
+    }
+});
+
+//part1
+// class Resize {
+
+//     constructor(target) {
+//       this.timeoutId;
+//       this.target = document.querySelector(target);
    
-       window.addEventListener("resize", this._resize.bind(this))
-     }
+//        window.addEventListener("resize", this._resize.bind(this))
+//      }
    
-     _resize() {
-       this.target.classList.add("is-resize");
-       clearTimeout(this.timeoutId);
+//      _resize() {
+//        this.target.classList.add("is-resize");
+//        clearTimeout(this.timeoutId);
        
-       this.timeoutId = setTimeout(() => {
-         this.target.classList.remove("is-resize");
-       }, 500);
-     }
+//        this.timeoutId = setTimeout(() => {
+//          this.target.classList.remove("is-resize");
+//        }, 500);
+//      }
    
-   }
+//    }
  
-   new Resize("html");
+//    new Resize("html");
