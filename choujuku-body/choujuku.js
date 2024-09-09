7b1c24117abaa8bb6fcf00ecd47b98b25ddf3702
yavaiyo <bsns.gh.etc@gmail.com>
2024-09-10 02:59:49 +0900

    未完

--- choujuku-body/choujuku.js
+++ choujuku-body/choujuku.js
@@ -1,6 +1,31 @@
-const Link = document.querySelector('.link-picture');
 const One = document.querySelector('.one');
+const Two = document.querySelector('.two');
+const Three = document.querySelector('.three');
+const Four = document.querySelector('.four');
+const Five = document.querySelector('.five');
+const Six = document.querySelector('.six');
+const Hone = document.querySelector('.hoverone');
+const Htwo = document.querySelector('.hovertwo');
+const Hthree = document.querySelector('.hoverthree');
+const Hfour = document.querySelector('.hoverfour');
+const Hfive = document.querySelector('.hoverfive');
+const Hsix = document.querySelector('.hoversix');
 
-Link.addEventListener('mouseover', () => {
+Hone.addEventListener('mouseover', () => {
     One.classList.toggle('active');
+})
+Htwo.addEventListener('mouseover', () => {
+    Two.classList.toggle('active');    
+})
+Hthree.addEventListener('mouseover', () => {
+    Three.classList.toggle('active');    
+})
+Hfour.addEventListener('mouseover', () => {
+    Four.classList.toggle('active');    
+})
+Hfive.addEventListener('mouseover', () => {
+    Five.classList.toggle('active');    
+})
+Hsix.addEventListener('mouseover', () => {
+    Six.classList.toggle('active');    
 })
